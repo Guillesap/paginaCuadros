7972e5a51af45f31615ee0cd82c69a9828bbfea0
WillyS <hsapag@hotmail.com>
2021-09-26 21:30:40 -0300

    [ADD]agregando otro ejemplo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//CLASE 5
+//CLASE N° 5 INCORPORAR OBJETOS
 class Compra{
     constructor(product, quantity, sending){
         this.product = product,
@@ -18,3 +18,19 @@ const send = prompt('Indique dirección para envio')
 const pedido1 = new Compra(prod, qt, send);
 
 alert(pedido1.Items());
+
+//OTRO EJEMPLO OBJETOS
+
+class Cuadro{
+    constructor(titulo, color){
+        this.title = titulo;
+        this.color = color;
+    }
+    mirar(){
+        console.log ("Este cuadro tiene como título" +  (this.title) + " y el marco es de color" +  (this.color) );
+    }
+}
+let Cuadro1 = new Cuadro ("Spider Man", "Marrón");
+Cuadro1.mirar();
+let Cuadro2 = new Cuadro ("Pelota de fútbol", "Blanco");
+Cuadro2.mirar();
