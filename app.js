4c500761493c66d168fbe94b65918fc209bead48
WillyS <hsapag@hotmail.com>
2021-10-09 02:45:16 -0300

    [UPPDATE]Boton con funcionalidad

--- app.js
+++ app.js
@@ -1,82 +1,28 @@
-//CLASE N° 6
+// EJEMPLO DE FORMULARIO PARA SELECCIONAR UN CUADRO ALMACENAMIENTO EN LOCALSTORAGE
 
-class Producto {
-    constructor (nombre, precio, cantidad) {
-        this.nombre = nombre;
-        this.precio = parseInt (precio);
-        this.cantidad = cantidad;
-        this.disponible = true;
-    }
-sumarIva(){
-    return this.precio * 1.21;
-}
-Vender(){
-    this.disponible = false;
-}
-}
-var arrayProductos = [];
-    do {
-        var comprobacion = prompt ('Ingrese nombre del Cuadro y una vez ingresada la cantidad de cuadros a adquirir, coloque la palabra FIN para terminar');
-        if (comprobacion === "fin" || comprobacion === "FIN" || comprobacion === "Fin"){
-            break;
-        }else{
-            nombreP = comprobacion;
-        var precioP = prompt ('Ingrese el precio del cuadro que desea adquirir');
-        var cantidadP = prompt ('Ingrese la cantidad de cuadros que desea adquirir');
-        arrayProductos.push(new Producto(nombreP, precioP, cantidadP));
-        }
-    }
-    while (comprobacion != "fin"|| comprobacion != "FIN"|| comprobacion != "Fin")
+window.onload = iniciar;
 
-    console.log (arrayProductos);
+// Botón "Click aquí", advierte el click del mouse del usuario.
 
-    for (var producto of arrayProductos) {
-        alert(" El Cuadro seleccionado es el llamado: "+producto.nombre+ "");
-        alert(" La cantidad solicitada es: "+producto.cantidad +"");
-        alert(" El precio  con IVA es $ "+producto.sumarIva() + "");
+function iniciar(){
+  let btnTexto = document.getElementById("btnTexto");  
+  btnTexto.addEventListener ("click", clickBtnTexto);
 
-    console.log(producto.nombre);
-    console.log(producto.cantidad);
-    console.log(producto.sumarIva());
-    }
-
-
-//CLASE 6 COMPLEMENTARIA
-class Compra{
-    constructor(product, quantity, shop){
-        return {
-            product : product,
-            quantity : parseInt(quantity),
-            shop : shop,
-            Items: () => {
-            return `Su pedido es el siguiente:  ${quantity} cuadro/s, cuyo/s nombre/s es/son: ${product} que será/n retirado/s de la Sucursal del Barrio ${shop}. Su pedido ha sido tomado. Gracias por Comprar en Estilo Bruder!`
-            }
-        }
-    }
+  //Renderiza notas
+  mostrarNotas ();
 }
 
-let cantidadProductos = parseInt(prompt('Cuantos cuadros diferentes desea adquirir?'))
+//funcion del boton: usuario escribe nombre de cuadro y  se almacena en el localStorage
+function clickBtnTexto() {
+ let txtNotas = document.getElementById("txtNotas");
 
-let counter = 0;
-const listadoPedidos = [];
-
-while(isNaN(cantidadProductos)){
-    cantidadProductos = parseInt(prompt('Debe ingresar un valor numerico. Cuantos cuadros diferentes desea comprar?'))
+ localStorage.cuadro = txtNotas.value;
+ mostrarNotas();
 }
 
-while(counter < cantidadProductos){
-    const prod = prompt('Ingrese el nombre del cuadro que desea comprar:')
-    const qt = parseInt(prompt('Cuantos de ese cuadro  desea comprar?'))
-    const store = prompt('En que sucursal de la Tienda desea retira?')
+ function mostrarNotas() {
+ let divNOtas = document.getElementById("divNotas");
 
-    const producto = new Compra(prod, qt, store);  
-    
-    listadoPedidos.push(producto)
-    console.log(producto.Items())
-    counter++;
+ //asigna las notas desde el LocalStarage
+ divNOtas.innerHTML = "Tu cuadro seleccionado es:  " + localStorage.cuadro;
 }
-
-listadoPedidos.sort((a, b) => (a.quantity > b.quantity) ? 1 : -1)
-
-console.log(listadoPedidos)
-
